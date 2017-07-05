790b96fceecc3b013b5fc8257c0a68f8649aa231
felix <2319979647@qq.com>
2017-07-05 18:40:01 +0800

    add babyBluetooth libray to deal with

---
+++ test/test_babyBlueTooth/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  test_babyBlueTooth
+//
+//  Created by Aka on 2017/7/5.
+//  Copyright © 2017年 forest. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"蓝牙";
+    
+    self.view.backgroundColor = [UIColor yellowColor];
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
