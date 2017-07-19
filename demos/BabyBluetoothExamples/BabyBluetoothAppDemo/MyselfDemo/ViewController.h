6f003049084f08623a8f77e49f1891209e9d674b
felix <2319979647@qq.com>
2017-07-19 14:46:51 +0800

    myself test for babybluetooth

---
+++ demos/BabyBluetoothExamples/BabyBluetoothAppDemo/MyselfDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyselfDemo
+//
+//  Created by Aka on 2017/7/19.
+//  Copyright © 2017年 刘彦玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demos/BabyBluetoothExamples/BabyBluetoothAppDemo/MyselfDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  MyselfDemo
+//
+//  Created by Aka on 2017/7/19.
+//  Copyright © 2017年 刘彦玮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+
+@end
