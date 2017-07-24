33ec333062ba3e11f31b4c7d937748505093462a
felix <2319979647@qq.com>
2017-07-24 18:08:44 +0800

    fix

--- test/testWKWebView/ViewController.m
+++ test/testWKWebView/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+
+@interface ViewController ()<WKNavigationDelegate,WKUIDelegate>
+
+@property (nonatomic, strong) WKWebView *webView;
 
 @end
 
@@ -18,12 +22,58 @@
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor grayColor];
+    
+    _webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:_webView];
+    _webView.UIDelegate = self;
+    _webView.navigationDelegate = self;
+    
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
 
+#pragma mark -- WKNavigationDelegate
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    
+}
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+    
+}
+
+- (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+    
+}
+
+- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler {
+    
+}
+
+- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView API_AVAILABLE(macosx(10.11), ios(9.0)) {
+    
 }
 
 
