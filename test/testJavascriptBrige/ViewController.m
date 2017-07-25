606fc635b2d154644173778521bf2b8b27b8108b
felix <2319979647@qq.com>
2017-07-25 12:32:28 +0800

    bridge core

--- test/testJavascriptBrige/ViewController.m
+++ test/testJavascriptBrige/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "WebViewJavascriptBridge.h"
+
+@interface ViewController ()<UIWebViewDelegate>
+
+@property (nonatomic, strong) WebViewJavascriptBridge *bridge;
+
+@property (nonatomic, strong) UIWebView *webView;
 
 @end
 
@@ -16,9 +22,88 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self loadBase];
+    
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+}
+
+- (void)loadBase {
+    
+    [WebViewJavascriptBridge enableLogging];
+    
+    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth([UIScreen mainScreen].bounds), CGRectGetHeight([UIScreen mainScreen].bounds)/2)];
+    _webView.delegate = self;
+    [self.view addSubview:_webView];
+    
+    _bridge = [WebViewJavascriptBridge bridgeForWebView:_webView];
+    [_bridge setWebViewDelegate:self];
+    [_bridge registerHandler:@"testObjcCallback" handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"objc callback with : %@",data);
+        responseCallback(data);
+    }];
+
+    [_bridge callHandler:@"testJavascriptHandler" data:@{@"foo":@"before ready"}];
+
+    [self renderButtons:_webView];
+    [self loadExamplePage:_webView];
+    
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    NSLog(@"webViewDidStartLoad");
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    NSLog(@"webViewDidFinishLoad");
+}
+
+- (void)renderButtons:(UIWebView*)webView {
+    
+    UIFont* font = [UIFont fontWithName:@"HelveticaNeue" size:11.0];
+    UIButton *callbackButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [callbackButton setTitle:@"Call handler" forState:UIControlStateNormal];
+    [callbackButton addTarget:self action:@selector(callHandler:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view insertSubview:callbackButton aboveSubview:webView];
+    callbackButton.frame = CGRectMake(0, 400, 100, 35);
+    callbackButton.titleLabel.font = font;
+    
+    UIButton* reloadButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [reloadButton setTitle:@"Reload webview" forState:UIControlStateNormal];
+    [reloadButton addTarget:webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+    [self.view insertSubview:reloadButton aboveSubview:webView];
+    reloadButton.frame = CGRectMake(90, 400, 100, 35);
+    reloadButton.titleLabel.font = font;
+    
+    UIButton* safetyTimeoutButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [safetyTimeoutButton setTitle:@"Disable safety timeout" forState:UIControlStateNormal];
+    [safetyTimeoutButton addTarget:self action:@selector(disableSafetyTimeout) forControlEvents:UIControlEventTouchUpInside];
+    [self.view insertSubview:safetyTimeoutButton aboveSubview:webView];
+    safetyTimeoutButton.frame = CGRectMake(190, 400, 120, 35);
+    safetyTimeoutButton.titleLabel.font = font;
+}
+
+- (void)disableSafetyTimeout {
+    [self.bridge disableJavscriptAlertBoxSafetyTimeout];
+}
+
+- (void)callHandler:(id)sender {
+    id data = @{ @"greetingFromObjC": @"Hi there, JS!" };
+    [_bridge callHandler:@"testJavascriptHandler" data:data responseCallback:^(id response) {
+        NSLog(@"testJavascriptHandler responded: %@", response);
+    }];
+}
+
+- (void)loadExamplePage:(UIWebView*)webView {
+    NSString* htmlPath = [[NSBundle mainBundle] pathForResource:@"ExampleApp" ofType:@"html"];
+    NSString* appHtml = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
+    NSURL *baseURL = [NSURL fileURLWithPath:htmlPath];
+    [webView loadHTMLString:appHtml baseURL:baseURL];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
