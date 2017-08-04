c9ea7299628beb8dbae47cc0d1fa6ad23956faef
felix <2319979647@qq.com>
2017-08-04 11:47:31 +0800

    test

---
+++ TestBluetooth/TestBluetooth/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  TestBluetooth
+//
+//  Created by Aka on 2017/8/4.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface ViewController ()<CBCentralManagerDelegate,CBPeripheralDelegate>
+
+@property (nonatomic)
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
