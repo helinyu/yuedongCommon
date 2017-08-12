ed12413be8eefc5ff740eebd92d080fcda1433fe
felix <2319979647@qq.com>
2017-08-12 18:18:57 +0800

    configure the page

---
+++ test_xcode_Framework/test_project/Test1/Test1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Test1ViewController.h
+//  test_project
+//
+//  Created by Aka on 2017/8/12.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test1ViewController : UIViewController
+
+@end
---
+++ test_xcode_Framework/test_project/Test1/Test1ViewController.m
@@ -0,0 +1,27 @@
+//
+//  Test1ViewController.m
+//  test_project
+//
+//  Created by Aka on 2017/8/12.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "Test1ViewController.h"
+
+@interface Test1ViewController ()
+
+@end
+
+@implementation Test1ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	NSLog(@"hello");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
