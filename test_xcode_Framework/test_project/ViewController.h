32cd2d98cad028ff7eda0768caaaa8cf2e558c99
felix <2319979647@qq.com>
2017-08-12 17:29:53 +0800

    outsid e

---
+++ test_xcode_Framework/test_project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_project
+//
+//  Created by Aka on 2017/8/12.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test_xcode_Framework/test_project/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test_project
+//
+//  Created by Aka on 2017/8/12.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
