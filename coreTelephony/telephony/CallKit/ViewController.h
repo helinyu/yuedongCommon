5ff560ace7b094c1fd706a19d8cbb37ee587fa57
felix <2319979647@qq.com>
2017-08-19 12:08:53 +0800

    callkit

---
+++ coreTelephony/telephony/CallKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CallKit
+//
+//  Created by Aka on 2017/8/19.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coreTelephony/telephony/CallKit/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  CallKit
+//
+//  Created by Aka on 2017/8/19.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CallKit/CallKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+    
+}
+
+
+@end
