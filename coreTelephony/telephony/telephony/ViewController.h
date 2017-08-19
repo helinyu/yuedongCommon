6e4102d8212970689c4973902ab660370cc0bdbc
felix <2319979647@qq.com>
2017-08-19 09:47:17 +0800

    test the telepheon init

---
+++ coreTelephony/telephony/telephony/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  telephony
+//
+//  Created by Aka on 2017/8/18.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coreTelephony/telephony/telephony/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  telephony
+//
+//  Created by Aka on 2017/8/18.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreTelephony/CoreTelephonyDefines.h>
+#import <CoreTelephony/CTCall.h>
+#import <CoreTelephony/CTCallCenter.h>
+#import <CoreTelephony/CTCarrier.h>
+#import <CoreTelephony/CTSubscriber.h>
+#import <CoreTelephony/CTCellularData.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTSubscriberInfo.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
