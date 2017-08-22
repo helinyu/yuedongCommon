6d11c2c5f52719ce1016b13ad60b85ce7d9d5215
felix <2319979647@qq.com>
2017-08-22 14:21:44 +0800

    test bock project

---
+++ Test_block/Test_block/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_block
+//
+//  Created by Aka on 2017/8/22.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
