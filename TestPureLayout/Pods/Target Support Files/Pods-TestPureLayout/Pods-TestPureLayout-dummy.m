0f9ebf83ac6d5619ade76f9a610503e53893fae8
felix <2319979647@qq.com>
2017-09-07 22:18:16 +0800

    test the purelayout

---
+++ TestPureLayout/Pods/Target Support Files/Pods-TestPureLayout/Pods-TestPureLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestPureLayout : NSObject
+@end
+@implementation PodsDummy_Pods_TestPureLayout
+@end
--- TestPureLayout/TestPureLayout/ViewController.m
+++ TestPureLayout/TestPureLayout/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "PureLayout.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [self comInit];
+}
+
+- (void)comInit {
+    UIView *view0 = [UIView newAutoLayoutView];
+    [self.view addSubview:view0];
+    view0.backgroundColor = [UIColor redColor];
+    [view0 autoCenterInSuperview];
+    [view0 autoSetDimensionsToSize:CGSizeMake(100, 100)];
+    
+    UIView *view1 = [UIView newAutoLayoutView];
+    [self.view addSubview:view1];
+    view1.backgroundColor = [UIColor yellowColor];
+    [view1 autoPinEdge:ALEdgeLeft toEdge:ALEdgeLeft ofView:view1.superview withOffset:NSLayoutRelationEqual];
+    [view1 autoPinEdge:ALEdgeTop toEdge:ALEdgeTop ofView:view1.superview withOffset:100.f];
+    [view1 autoSetDimensionsToSize:CGSizeMake(100,100)];
+    
 }
 
 
