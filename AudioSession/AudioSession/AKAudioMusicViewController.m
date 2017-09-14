815b6d60ae126c5d0bbfa3a4823d5e4499d8484b
felix <2319979647@qq.com>
2017-09-14 10:33:12 +0800

    audio session for speech & audio player

--- AudioSession/AudioSession/AKAudioMusicViewController.m
+++ AudioSession/AudioSession/AKAudioMusicViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AKAudioMusicViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface AKAudioMusicViewController ()
 
@@ -16,22 +17,65 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self comInit];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)comInit {
+    UIButton *playBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [playBtn setTitle:@"播放(长)音频" forState:UIControlStateNormal];
+    [playBtn addTarget:self action:@selector(onAudioMusicClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:playBtn];
+    playBtn.frame = CGRectMake(100, 100, 200, 30);
+    
+    UIButton *openSessionBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [openSessionBtn setTitle:@"open audiosession" forState:UIControlStateNormal];
+    [openSessionBtn addTarget:self action:@selector(onOpenSessionClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:openSessionBtn];
+    openSessionBtn.frame = CGRectMake(100, 200, 200, 30);
+    
+    UIButton *closeSessionBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [closeSessionBtn setTitle:@"close audiosession" forState:UIControlStateNormal];
+    [closeSessionBtn addTarget:self action:@selector(onCloseSessionClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:closeSessionBtn];
+    closeSessionBtn.frame = CGRectMake(100, 300, 200, 30);
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark -- custom btn method
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)onOpenSessionClick:(UIButton *)sender {
+    NSLog(@"开启音频回话");
+    NSError *error = nil;
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    BOOL flag = [[AVAudioSession sharedInstance] setActive:YES error:&error];
+    if (!flag) {
+        NSLog(@"error : %@",error);
+    }
 }
-*/
+
+- (void)onCloseSessionClick:(UIButton *)sender {
+    NSLog(@"关闭音频回话");
+    AudioSessionSetActiveWithFlags(NO, kAudioSessionSetActiveFlag_NotifyOthersOnDeactivation);
+    NSError *error = nil;
+    BOOL result = [[AVAudioSession sharedInstance] setActive:NO error:&error];
+    if (error) {
+        NSLog(@"error set active of audio session : %@",error);
+    }
+}
+
+- (void)onAudioMusicClick:(UIButton *)sender {
+    NSLog(@"播放音频");
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"yekongzhongzuiliangdexing" ofType:@"mp3"];
+    NSError *error = nil;
+    AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:path] error:&error];
+    if (error) {
+        NSLog(@"error :%@",error);
+    }
+    [player prepareToPlay];
+    [player play];
+    
+    
+}
 
 @end
--- AudioSession/AudioSession/AKTextSpeakerViewController.m
+++ AudioSession/AudioSession/AKTextSpeakerViewController.m
@@ -7,6 +7,13 @@
 //
 
 #import "AKTextSpeakerViewController.h"
+#import "AKSpeechMgr.h"
+#import "AKSpeechModel.h"
+//#import "MSLog.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AudioToolbox/AudioSession.h>
 
 @interface AKTextSpeakerViewController ()
 
@@ -16,22 +23,65 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self comInit];
+    
+}
+
+- (void)comInit {
+    UIButton *playBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [playBtn setTitle:@"播放语音" forState:UIControlStateNormal];
+    [playBtn addTarget:self action:@selector(onSpeakerClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:playBtn];
+    playBtn.frame = CGRectMake(100, 100, 200, 30);
+    
+    UIButton *openSessionBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [openSessionBtn setTitle:@"open audiosession" forState:UIControlStateNormal];
+    [openSessionBtn addTarget:self action:@selector(onOpenSessionClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:openSessionBtn];
+    openSessionBtn.frame = CGRectMake(100, 200, 200, 30);
+    
+    UIButton *closeSessionBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [closeSessionBtn setTitle:@"close audiosession" forState:UIControlStateNormal];
+    [closeSessionBtn addTarget:self action:@selector(onCloseSessionClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:closeSessionBtn];
+    closeSessionBtn.frame = CGRectMake(100, 300, 200, 30);
+
+}
+
+#pragma mark -- custom btn method
+
+- (void)onOpenSessionClick:(UIButton *)sender {
+    NSLog(@"开启音频回话");
+    NSError *error = nil;
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+   BOOL flag = [[AVAudioSession sharedInstance] setActive:YES error:&error];
+    if (!flag) {
+        NSLog(@"error : %@",error);
+    }
+}
+
+- (void)onCloseSessionClick:(UIButton *)sender {
+    NSLog(@"关闭音频回话");
+    AudioSessionSetActiveWithFlags(NO, kAudioSessionSetActiveFlag_NotifyOthersOnDeactivation);
+    NSError *error = nil;
+    BOOL result = [[AVAudioSession sharedInstance] setActive:NO error:&error];
+    if (error) {
+        NSLog(@"error set active of audio session : %@",error);
+    }
+}
+
+- (void)onSpeakerClick:(UIButton *)sender {
+    AKSpeechModel *item = [AKSpeechModel new];
+    item.contentText = @"先说说iOS应用程序5个状态：停止运行-应用程序已经终止，或者还未启动。 ... 着当应用退至后台，其后台运行仅能持续10分钟便会转至休眠状态。iOS ... 不过拥有了这个接口后，这情况将不复存在，以后推送将能够直接启动后台任务";
+    [[AKSpeechMgr shared] speechWithItem:item complete:^(AVSpeechSynthesizer *synthesizer, AVSpeechUtterance *utterance, NSRange characterRange, AKASpeechDelegateType type) {
+//        NSLog(@"text :%@",[utterance.speechString substringWithRange:characterRange]);
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
