07cffbb803bdfbfec08951759ff25d36ef6d3538
felix <2319979647@qq.com>
2017-09-20 18:11:05 +0800

    web audio

--- VideoBackgroundModel/VideoBackgroundModel/ViewController.m
+++ VideoBackgroundModel/VideoBackgroundModel/ViewController.m
@@ -10,9 +10,11 @@
 #import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController () {
-    AVPlayer *_player;
+    AVAudioPlayer *_player;
     BOOL _isPlayingNow;
 }
+
+@property (nonatomic, strong) UIWebView *webView;
 
 @end
 
@@ -20,19 +22,107 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self comInit];
+}
+
+- (void)comInit {
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn setTitle:@"播放音频" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(onAudioPlayClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    btn.frame = CGRectMake(100, 100, 100, 30);
     
-    //    设置后台播放
+    UIButton *stopBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [stopBtn setTitle:@"停止播放" forState:UIControlStateNormal];
+    [stopBtn addTarget:self action:@selector(onStopPlayClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:stopBtn];
+    stopBtn.frame = CGRectMake(100, 150, 100, 30);
+    
+    UIButton *webBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [webBtn setTitle:@"网页加载" forState:UIControlStateNormal];
+    [webBtn addTarget:self action:@selector(onWebClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:webBtn];
+    webBtn.frame = CGRectMake(210, 100, 100, 30);
+    
+    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 200, CGRectGetWidth([UIScreen mainScreen].bounds), CGRectGetHeight([UIScreen mainScreen].bounds)-200.f)];
+    _webView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:_webView];
+    
+}
+
+- (void)onWebClick:(UIButton *)sender {
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.33.97:8000/movie/audio.html"]]];
+}
+
+- (void)onStopPlayClick:(id)sender {
+    NSLog(@"停止播放");
+    [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
+    [_player stop];
+    _player = nil;
+    NSError *error = nil;
+    [[AVAudioSession sharedInstance] setActive:NO withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:&error];
+    if (error) {
+        NSLog(@"error");
+    }
+}
+
+- (void)onAudioPlayClick:(id)sender {
+
+//    NSError *cateforyError = nil;
+//    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategory error:&cateforyError];
+//    if (cateforyError) {
+//        NSLog(@"erorro :%@",cateforyError);
+//    }
+//    
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    NSLog(@"开始播放");
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
-    
-    //    设置播放器
-    
     NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"那些花儿" ofType:@"mp3"] ];
-    _player = [[AVPlayer alloc] initWithURL:url];
+//    _player = [[AVPlayer alloc] initWithURL:url];
+    _player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
     [_player play];
     _isPlayingNow = YES;
     
-    //后台播放显示信息设置
     [self setPlayingInfo];
+//    [self _createRemoteCommandCenter];
+}
+
+- (void)_createRemoteCommandCenter {
+    NSLog(@"cmd center");
+    if ([UIDevice currentDevice].systemVersion.floatValue >=7.1f) {
+        MPRemoteCommandCenter *cmdCenter = [MPRemoteCommandCenter sharedCommandCenter];
+        
+        [cmdCenter.playCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+            NSLog(@"播放");
+            return MPRemoteCommandHandlerStatusSuccess;
+        }];
+        
+        [cmdCenter.pauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+            NSLog(@"暂停播放");
+            return MPRemoteCommandHandlerStatusSuccess;
+        }];
+        
+        [cmdCenter.nextTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+            NSLog(@"下一首");
+            return MPRemoteCommandHandlerStatusSuccess;
+        }];
+        
+        [cmdCenter.previousTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+            NSLog(@"上一首");
+            return MPRemoteCommandHandlerStatusSuccess;
+        }];
+        
+        [cmdCenter.changePlaybackPositionCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+            MPChangePlaybackPositionCommandEvent *positionEvent = (MPChangePlaybackPositionCommandEvent *)event;
+            NSTimeInterval positionTime = positionEvent.positionTime;
+            NSTimeInterval timeScale = 30.f;
+            NSLog(@"change position time ;%f",positionTime);
+            return MPRemoteCommandHandlerStatusSuccess;
+        }];
+        
+    }else{
+        //        7.0 的需要进行处理
+    }
 }
 
 #pragma mark - 接收方法的设置
@@ -76,19 +166,17 @@
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    //    接受远程控制
+    //接受远程控制
     [self becomeFirstResponder];
-    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
-    //    取消远程控制
+    //取消远程控制
     [self resignFirstResponder];
     [[UIApplication sharedApplication] endReceivingRemoteControlEvents];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
