44210e83566df2406a694f736141babaf1579176
felix <2319979647@qq.com>
2017-11-05 18:17:22 +0800

    init ffmpeg test workspace

---
+++ FFMpegForIOSDemo/FFMpegForIOSDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FFMpegForIOSDemo
+//
+//  Created by Aka on 2017/11/5.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
