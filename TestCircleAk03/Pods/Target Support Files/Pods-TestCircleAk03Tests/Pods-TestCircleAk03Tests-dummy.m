4725f06f42e28a67f60cc4e592eb63160afcf0c5
felix <2319979647@qq.com>
2017-11-21 16:38:26 +0800

    init

---
+++ TestCircleAk03/Pods/Target Support Files/Pods-TestCircleAk03Tests/Pods-TestCircleAk03Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestCircleAk03Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TestCircleAk03Tests
+@end
---
+++ TestCircleAk03/Pods/Target Support Files/Pods-TestCircleAk03UITests/Pods-TestCircleAk03UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestCircleAk03UITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestCircleAk03UITests
+@end
