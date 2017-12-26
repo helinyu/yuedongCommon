1a2eea4f9fa58430c2512fa3c5cf44b379460cf4
felix <2319979647@qq.com>
2017-12-26 19:32:43 +0800

    shadow add must not be click to bounds

---
+++ HoverButton/HoverButton/4_base/YD4ViewController.h
@@ -0,0 +1,13 @@
+//
+//  YD4ViewController.h
+//  HoverButton
+//
+//  Created by Aka on 2017/12/26.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YD4ViewController : UIViewController
+
+@end
---
+++ HoverButton/HoverButton/4_base/YD4ViewController.m
@@ -0,0 +1,37 @@
+//
+//  YD4ViewController.m
+//  HoverButton
+//
+//  Created by Aka on 2017/12/26.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "YD4ViewController.h"
+
+@interface YD4ViewController ()
+
+@end
+
+@implementation YD4ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
