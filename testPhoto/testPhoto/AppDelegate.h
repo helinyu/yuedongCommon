a1aa4286d3ab92a877ab895d88c5569538b8125a
felix <2319979647@qq.com>
2017-12-29 19:01:12 +0800

    test photo

---
+++ testPhoto/testPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testPhoto
+//
+//  Created by Aka on 2017/12/29.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
