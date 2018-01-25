7d267cd7b7486cffd830fb66d1a4e0d0a214ecb0
helinyu <2319979647@qq.com>
2018-01-25 13:39:51 +0800

    fix the tranns

--- TeatViewAttribute/TeatViewAttribute/ViewController.m
+++ TeatViewAttribute/TeatViewAttribute/ViewController.m
@@ -20,6 +20,8 @@
 
 @property (nonatomic, strong) GHCustomView *customView;
 
+@property (nonatomic, strong) UIImageView *imgView;
+
 @end
 
 @implementation ViewController
@@ -27,10 +29,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self test1];
+//    [self test1];
+    
+    [self test2];
+}
+
+- (void)test2 {
+    _btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview:_btn];
+    _btn.backgroundColor = [UIColor redColor];
+    [_btn setTitle:@"点击" forState:UIControlStateNormal];
+    _btn.frame = CGRectMake(0, 64, 100, 30);
+    [_btn addTarget:self action:@selector(onTapClick:) forControlEvents:UIControlEventTouchUpInside];
+    
+    _imgView = [UIImageView new];
+    [self.view addSubview:_imgView];
+    self.view.backgroundColor = [UIColor yellowColor];
+    _imgView.image = [UIImage imageNamed:@"Snip20180125_1.png"];
+    _imgView.frame = CGRectMake(100, 100, 200, 100);
 }
 
 - (void)test1 {
+    
+  
     
     _customView = [[GHCustomView alloc] initWithFrame:CGRectMake(0, 100, 200, 300)];
     [self.view addSubview:_customView];
@@ -69,23 +90,57 @@
 //    CGSize size1 = _subView0.intrinsicContentSize;
 //    NSLog(@"size ; %f",size.height);
     
-    CGSize contentSize = _subLabel.intrinsicContentSize;
-    CGSize boundsize = _subLabel.bounds.size;
-    CGSize framesize = _subLabel.frame.size;
-    if ([_subLabel.text isEqualToString:@"kadsdjfks"]) {
-        _subLabel.text = @"23";
-    }else {
-        _subLabel.text =@"kadsdjfks" ;
-    }
-    [_subLabel sizeToFit];
-
-    CGSize contentSize1 = _subLabel.intrinsicContentSize;
-    CGSize boundsize1 = _subLabel.bounds.size;
-    CGSize framesize11 = _subLabel.frame.size;
+//    CGSize contentSize = _subLabel.intrinsicContentSize;
+//    CGSize boundsize = _subLabel.bounds.size;
+//    CGSize framesize = _subLabel.frame.size;
+//    if ([_subLabel.text isEqualToString:@"kadsdjfks"]) {
+//        _subLabel.text = @"23";
+//    }else {
+//        _subLabel.text =@"kadsdjfks" ;
+//    }
+//    [_subLabel sizeToFit];
+//
+//    CGSize contentSize1 = _subLabel.intrinsicContentSize;
+//    CGSize boundsize1 = _subLabel.bounds.size;
+//    CGSize framesize11 = _subLabel.frame.size;
     
-    
+    [self onTest2];
     
     NSLog(@"size");
+}
+
+- (void)onTest2 {
+    
+//     下右 移动100px
+//    _imgView.transform = CGAffineTransformIdentity; // 默认是不变的
+//    _imgView.transform = CGAffineTransformMakeTranslation(100.f, 100.f); // 像右、下移动了100.f     t' = [ 1 0 0 1 100.f 100.f ] */ frame 没有变化，还是原来的大小 （所以，平移之后再进行平移，是回到原来的位置进行平移）
+//    _imgView.transform = CGAffineTransformMakeTranslation(0.f, 50); // 像右、下移动了100.f     t' = [ 1 0 0 1 100.f 100.f ] */ frame 没有变化，还是原来的大小 （所以，平移之后再进行平移，是回到原来的位置进行平移）
+
+    //     x/y 轴线上的缩放 （）
+//    _imgView.transform = CGAffineTransformMakeScale(0.5, 3);
+//     注意这些变换都是只能够执行一个（后面的会被前面的覆盖掉）
+    
+
+    CGAffineTransform rotate =CGAffineTransformMakeRotation(M_PI_2);
+    CGAffineTransform scale = CGAffineTransformMakeScale(0.5, 3);
+    //     旋转 （角度）
+//    _imgView.transform = rotate;
+    
+//    BOOL flag = CGAffineTransformIsIdentity(CGAffineTransformIdentity);
+//  NSLog(@"falg :%zd",flag);
+    
+    CGAffineTransform firstTrans =   CGAffineTransformMakeTranslation(100.f, 100.f);
+//    CGAffineTransform secondTrans = CGAffineTransformTranslate(firstTrans,100,100);
+//    CGAffineTransform invertTrans = CGAffineTransformInvert(secondTrans);
+//   CGAffineTransform concatTrans =
+//    _imgView.transform = CGAffineTransformConcat(rotate,scale); //两个效果
+//    CGAffineTransformEqualToTransform
+    
+   CGPoint point = CGPointApplyAffineTransform(_imgView.center, firstTrans);
+//     变换之后的位置 (获取属性)
+   CGRect rect = CGRectApplyAffineTransform(_imgView.frame, firstTrans);
+    
+    NSLog(@"falg :");
 }
 
 - (void)didReceiveMemoryWarning {
