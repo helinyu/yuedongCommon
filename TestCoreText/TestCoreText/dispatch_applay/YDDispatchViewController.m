4120f88d3a69a3db78f072c028d64cee127398c7
felix <2319979647@qq.com>
2018-03-06 00:54:22 +0800

    fix

--- TestCoreText/TestCoreText/dispatch_applay/YDDispatchViewController.m
+++ TestCoreText/TestCoreText/dispatch_applay/YDDispatchViewController.m
@@ -17,20 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.view.backgroundColor = [UIColor redColor];
+    
     [self test0];
 }
 
 - (void)test0 {
     NSArray *array = @[@1,@2,@3,@4,@5,@6,@7,@8,@9,@10,@11,@12,@13];
-    dispatch_queue_t queue = dispatch_queue_create(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(queue, ^{
-        dispatch_apply([array count], queue, ^(size_t index) {
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+//    dispatch_async(queue, ^{
+//    放在主线程（当前）就会出现错误，这个一定不要放在当前的线程，一定异步到其他线程（应该是这样）
+//        dispatch_apply([array count], dispatch_get_main_queue(), ^(size_t index) { 错误
+//这个可以在主线程的，会执行完block里面的内容在进行你处理其他的；
+//    网络请求上都是用group，是否也可以使用这个？有待验证
+    dispatch_apply([array count], queue, ^(size_t index) {
             NSLog(@" %zu: %@",index, [array objectAtIndex:index]);
         });
-        dispatch_async(dispatch_get_main_queue(), ^{
-            NSLog(@"asy queue");
-        });
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"asy queue");
     });
+//    });
     NSLog(@"main done ouitside");
 }
 
