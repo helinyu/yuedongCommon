09686bc565f8401a78f5bf5b111234fa555aea99
felix <2319979647@qq.com>
2018-05-30 00:15:28 +0800

    snap

---
+++ test_snapkit/test_snapkit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_snapkit
+//
+//  Created by Aka on 2018/5/30.
+//  Copyright © 2018年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_snapkit/test_snapkit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_snapkit
+//
+//  Created by Aka on 2018/5/30.
+//  Copyright © 2018年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
